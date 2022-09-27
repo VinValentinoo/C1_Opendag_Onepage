0382ff91fe533f3ff5ee29b8df02123e26864aba
Vin Valentino <42009589+VinValentinoo@users.noreply.github.com>
2022-09-27 16:16:20 +0200

    Subjecs added

--- js/app.js
+++ js/app.js
@@ -4,11 +4,12 @@ const curio = intro.querySelector("h1.curio");
 const dev = intro.querySelector("h1.dev");
 const kerrie = intro.querySelector("img.kerrie")
 //END SECTION
-const section = document.querySelector("section");
-const end = section.querySelector("h1");
+const section = document.querySelector("section.vak-codelab");
+const vakText = section.querySelector("h1.vakT");
 
 //SCROLLMAGIC
 const controller = new ScrollMagic.Controller();
+const vakController = new ScrollMagic.Controller();
 
 //Scenes
 let scene = new ScrollMagic.Scene({
@@ -55,6 +56,58 @@ let kerstmanAnim = new ScrollMagic.Scene({
   .setTween(kerstmanImgSlide)
   .addTo(controller);
 
+
+// Section 1
+// Vakken text fade out
+
+const sectionAnim = TweenMax.fromTo(section, 3 , {backgroundColor:'black'}, {duration: 3, backgroundColor: 'whitesmoke', ease:"linear"});
+
+let scene2 = new ScrollMagic.Scene({
+  duration: 10000,
+  triggerElement: section,
+  triggerHook: 0,
+})
+  .addIndicators()
+  .setTween(sectionAnim)
+  .setPin(section)
+  .addTo(vakController)
+
+
+// Text animation vakken
+
+const vakTextAnim = TweenMax.fromTo(vakText, 3 , {opacity: 1}, {opacity: 0});
+
+let vakT = new ScrollMagic.Scene({
+  duration: 7000,
+  triggerElement: section,
+  triggerhook: 0
+})
+  .setTween(vakTextAnim)
+  .addTo(vakController);
+
+// Codelab text
+const codelab = section.querySelector("h2.codelab");
+const codelabT = section.querySelector("h2.codelabT");
+
+const codelabFadeIn = TweenMax.fromTo(codelab, 3, {opacity: 1}, {opacity: 0});
+const codelabTFadeIn = TweenMax.fromTo(codelabT, 3, {opacity: 1}, {opacity: 0});
+
+let codelabTitel = new ScrollMagic.Scene({
+  duration: 10000,
+  triggerElement: section,
+  triggerhook: 0,
+})
+  .setTween(codelabFadeIn)
+  .addTo(vakController);
+
+let codelabText = new ScrollMagic.Scene({
+  duration: 10000,
+  triggerElement: section,
+  triggerhook: 0
+})
+  .setTween(codelabTFadeIn)
+  .addTo(vakController);
+
 //Video Animation
 let accelamount = 0.1;
 let scrollpos = 0;
@@ -66,7 +119,6 @@ scene.on("update", e => {
 
 setInterval(() => {
   delay += (scrollpos - delay) * accelamount;
-  console.log(scrollpos, delay);
 
   video.currentTime = delay;
 }, 33.3);
